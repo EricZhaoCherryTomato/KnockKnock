6d76bd3e25c4aa2634075d04dc4c36f59ca58373
Eric.Zhao <Eric.Zhao@dominos.com.au>
2017-01-13 15:04:50 +1000

    Init Commit

---
+++ KnockKnock/KnockKnock.Tests.Unit/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KnockKnock;
+using KnockKnock.Controllers;
+
+namespace KnockKnock.Tests.Unit.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
